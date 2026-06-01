0e2e0a77e9fcc10cfb197deb0766da19dd0e59d5
Manikanta <mani.manikanta@gmail.com>
2026-06-01 14:39:24 +0530

    list_indexing2.py

---
+++ list_indexing2.py
@@ -0,0 +1,7 @@
+programming_languages_list = ["Python", "Java", "Ruby", "C", "C++", "Go", "R", "JavaScript", "Swift", "PHP", "Kotlin", "Perl"]
+# Write your code here
+a = int(input())
+for i in range(a):
+    b = int(input())
+    print(programming_languages_list[b])
+
