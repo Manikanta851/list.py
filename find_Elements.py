fe98e7ae1784dc64a14199ddc3963a7c3c867ecb
Manikanta <mani.manikanta@gmail.com>
2026-06-01 14:58:27 +0530

    find_Elements.py

---
+++ find_Elements.py
@@ -0,0 +1,10 @@
+L = ["5", "eat", "9.80", "Water", "python", "-678", "7685.26", "-2.5", "sing"]
+
+# Write your code here
+s = input()
+for i in L:
+    if s == i :
+        print("True")
+        break 
+else :
+    print("False")
